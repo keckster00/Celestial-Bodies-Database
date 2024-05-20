6bfe2cfcf4f37f66b1a596a86cda79796f85841d
Drew Keck <78997151+keckster00@users.noreply.github.com>
2024-05-20 10:57:55 -0400

    Update salon.sh

--- Salon-Appointment-Scheduler/salon.sh
+++ Salon-Appointment-Scheduler/salon.sh
@@ -8,26 +8,53 @@ MAIN_MENU() {
     echo -e "\n$1"
   fi
 
-  SERVICES_LIST=$($PSQL "SELECT * FROM services")
-  echo "$SERVICES_LIST" | while read SERVICE_ID BAR SERVICE
+  #prints the list of services
+  SERVICES=$($PSQL "SELECT * FROM services")
+  echo -e "\nHere are our services:"
+  echo "$SERVICES" | while read SERVICE_ID BAR SERVICE_NAME
   do
-    echo "$SERVICE_ID) $SERVICE"
+    echo "$SERVICE_ID) $SERVICE_NAME"
   done
 
   echo -e "\nWhat service would you like?"
-  read SERVICE_ID
+  read SERVICE_ID_SELECTED
 
-  if [[ ! $SERVICE_ID =~ ^[0-9]+$ ]]
+  #if they don't input a number
+  if [[ ! $SERVICE_ID_SELECTED =~ ^[0-9]+$ ]]
   then
     MAIN_MENU "That's not a number, choose again."
   fi
 
-  SERVICE_ID_RESULT=$($PSQL "SELECT service_id FROM services WHERE service_id = $SERVICE_ID")
+  # Service doesn't exist
+  SERVICE_ID_RESULT=$($PSQL "SELECT service_id FROM services WHERE service_id = $SERVICE_ID_SELECTED")
   if [[ -z $SERVICE_ID_RESULT ]]
   then
-    MAIN_MENU "Please choose a valid service."
+    MAIN_MENU "Service doesn't exist."
   fi
-  fre
+
+  #request user number
+  echo -e "\nWhat's your phone number?:"
+  read CUSTOMER_PHONE
+  #if customer not in database
+  PHONE_NUMBER_RESULT=$($PSQL "SELECT phone FROM customers WHERE phone = '$CUSTOMER_PHONE'")
+  if [[ -z $PHONE_NUMBER_RESULT ]]
+  then
+    # insert into database
+    echo -e "\nWhat's your name?:"
+    read CUSTOMER_NAME
+    CUSTOMER_INSERT_RESULT=$($PSQL "INSERT INTO customers(name, phone) VALUES('$CUSTOMER_NAME', '$CUSTOMER_PHONE')")
+  fi
+
+  # get customer ID
+  CUSTOMER_ID=$($PSQL "SELECT customer_id FROM customers WHERE phone = '$CUSTOMER_PHONE'")
+  CUSTOMER_NAME=$($PSQL "SELECT name FROM customers WHERE customer_id = $CUSTOMER_ID")
+  # ask for time of appointmnet
+  echo -e "\nWhat time do you want the appointment?:"
+  read SERVICE_TIME
+  # insert into appointments
+  APPOINTMENT_INSERT_RESULT=$($PSQL "INSERT INTO appointments(customer_id, service_id, time) VALUES($CUSTOMER_ID, $SERVICE_ID_SELECTED, '$SERVICE_TIME')")
+  SERVICE_NAME=$($PSQL "SELECT name FROM services WHERE service_id = $SERVICE_ID_SELECTED")
+  MAIN_MENU "I have put you down for a$SERVICE_NAME at $SERVICE_TIME,$CUSTOMER_NAME."
 }
 
 MAIN_MENU
